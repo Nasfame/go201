93126e56acd5af412f22c72d6307823feb2b4038
Hiro <laciferin@gmail.com>
2023-06-17 10:22:14 +0530

    goup install

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ go install github.com/cosmtrek/air@latest
 sudo apt update -y
 sudo apt install tmux -y
 
+curl -sSf https://raw.githubusercontent.com/owenthereal/goup/master/install.sh | sh -s -- '--skip-prompt'
+
+goup install
+
+goup install tip
+
+
