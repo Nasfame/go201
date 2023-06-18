e90ed5e1b22ee7d0f57f4ff2775acbbdeddde839
Hiro <laciferin@gmail.com>
2023-06-18 03:53:30 +0000

    fix server

--- wasm/server/main.go
+++ wasm/server/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {  
     fmt.Println("starting server")
-    err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../")))
+    err := http.ListenAndServe(":9090", http.FileServer(http.Dir(".")))
     if err != nil {
         fmt.Println("Failed to start server", err)
         return
