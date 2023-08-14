31c4e998e73513fa808dd1f0cacb4e40e57ab427
Hiro <laciferin@gmail.com>
2023-08-14 21:16:38 +0000

    issues: add

--- issues/i61992.go
+++ issues/i61992.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
 
 type S1 struct {
-	a, b, c []int
-	i       int
+	a, b, c []int //important removing this removes ICE
+
+	i int
 }
 
 type S2 struct {
@@ -15,10 +17,9 @@ func F(i int, f func(S1, S2, int) int) int {
 	return f(
 		S1{},
 		S2{m: map[int]int{}},
-		i<<2,
-	)
+		1<<i)
 }
 
 func main() {
-	println("Hello, Hiro")
+	fmt.Println("Hello, Hiro")
 }
