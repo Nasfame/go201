016c4acdd5da4a915cfd9cee034cfd2db0b39fea
Hiro <laciferin@gmail.com>
2023-08-14 19:05:36 +0000

    remove unnecessary pkg

--- issues/i61992.go
+++ issues/i61992.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
 
 type S1 struct {
 	a, b, c []int
@@ -19,15 +15,10 @@ func F(i int, f func(S1, S2, int) int) int {
 	return f(
 		S1{},
 		S2{m: map[int]int{}},
-		1>>i,
+		i<<2,
 	)
 }
 
 func main() {
-	println(runtime.Version())
-	println(F(10, func(s1 S1, s2 S2, i int) int {
-		fmt.Println(s1, s2, i)
-		return i
-	}))
 	println("Hello, Hiro")
 }
