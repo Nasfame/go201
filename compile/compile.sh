945fa9f8c8d27d3bbc7dbe7ad19896b8df3951c5
Hiro <laciferin@gmail.com>
2023-08-17 23:05:27 +0000

    compile.a:...

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,10 +1,12 @@
 go tool compile -o compile.o compile.go
-chmod +x compile.o
+# chmod +x compile.o
 # ./compile.o
-go build compile.o
+# go build compile.o
 
 
 # go build -o x.out compile.go
 # ./x.out
 
-   go tool pack r main.a main.o
+go tool pack r compile.a compile.o
+
+go tool link -o compile compile.o
