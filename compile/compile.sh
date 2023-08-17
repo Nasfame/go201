a8c03b42304cc5485f97784f103bb42b5ae0de8a
Hiro <laciferin@gmail.com>
2023-08-17 23:03:07 +0000

    remove files

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,6 +1,10 @@
 go tool compile -o compile.o compile.go
 chmod +x compile.o
-./compile.o
+# ./compile.o
+go build compile.o
 
-go build -o x.out compile.go
-./x.out
+
+# go build -o x.out compile.go
+# ./x.out
+
+   go tool pack r main.a main.o
