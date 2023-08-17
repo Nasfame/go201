da7e3c3a354f22d7e6c75ef17befe62950a94403
Hiro <laciferin@gmail.com>
2023-08-17 23:14:35 +0000

    a:

--- compile/compile.sh
+++ compile/compile.sh
@@ -7,6 +7,9 @@ go tool compile -o compile.o compile.go
 # go build -o x.out compile.go
 # ./x.out
 
+# Linking and building an executable: If you have multiple object files or packages that need to be linked together, you can use the go tool link command to link the object files and create an executable binary. Here's an example:
+# go tool link -o executable main.o x.o
+
+# Creating a package archive: If the object file represents a package that you want to distribute as a library, you can create a package archive (.a file) using the go tool pack command.
 go tool pack r compile.a compile.o
 
-go tool link -o compile compile.o
