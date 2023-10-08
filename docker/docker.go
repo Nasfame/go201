5037d49e8ee4c8d18e2e0f69cd5e368984e34e42
Nasfame <laciferin@gmail.com>
2023-10-09 03:59:20 +0530

    fixes: docker script

--- docker/docker.go
+++ docker/docker.go
@@ -1,45 +1,50 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/docker/docker/api/types/container"
-    "github.com/docker/docker/client"
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 func main() {
-    ctx := context.Background()
-    client, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	ctx := context.Background()
+	client, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    config := &container.Config{
-        Image: "ubuntu:latest",
-        Cmd:    []string{"bash"},
-    }
+	config := &container.Config{
+		Image: "ubuntu:latest",
+		Cmd:   []string{"bash"},
+	}
 
-    container, err := client.ContainerCreate(ctx, config, nil, nil, "")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	container, err := client.ContainerCreate(ctx, config, nil, nil, &ocispec.Platform{
+		OS: "Linux",
+	}, "casual")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    err = client.ContainerStart(ctx, container.ID)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	err = client.ContainerStart(ctx, container.ID, types.ContainerStartOptions{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    statusCh, errCh := client.ContainerWait(ctx, container.ID, container.WaitConditionNotRunning)
-    select {
-    case err := <-errCh:
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-    case status := <-statusCh:
-        fmt.Println("Container exited with status:", status)
-    }
+	statusCh, errCh := client.ContainerWait(ctx, container.ID, "removed")
+	select {
+	case err := <-errCh:
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	case status := <-statusCh:
+		fmt.Println("Container exited with status:", status)
+	}
 }
